6d570a92f948f16f9bca1c527d09b4f30bd3dc23
Vixo341 <malpa341@gmail.com>
2020-04-14 14:07:17 +0200

    Test get/post nodejs and angular

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
 
-app.use('/api/posts', (req, res, next) => {
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-with, Content-Type, Accept"
+     );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+     "GET, POST, PATCH, DELETE, OPTIONS"
+     );
+  next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Test post Added Successfully!'
+  });
+});
+
+
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     {id: '1',
      title: 'test backend title',
