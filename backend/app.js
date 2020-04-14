c6dcfcfef2ffb078ad2ccbc0b8e0264fbe889f36
Vixo341 <malpa341@gmail.com>
2020-04-14 13:35:50 +0200

    test backend connection

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
 
-app.use((req, res, next) => {
-  res.send('Test Express');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {id: '1',
+     title: 'test backend title',
+     content: 'test backend content'}
+  ];
+   res.status(200).json({
+    message: 'Backend Post Fetched succesfully!',
+    posts: posts
+    });
   });
 
  module.exports = app;
